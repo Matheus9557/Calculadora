89fdf45d3a7e6acd7911bfd13b82129666c87c5e
Matheus9557 <79982182+Matheus9557@users.noreply.github.com>
2021-07-31 12:23:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,73 @@ function calcular() {
     document.getElementById("resultado").innerHTML = "null";
   }
 }
+document.addEventListener("keypress", function (event) {
+  if (event.key === "=") {
+    const btn = document.querySelector("#igual");
+    btn.click();
+  }
+  if (event.key === "+") {
+    const btn = document.querySelector("#soma");
+    btn.click();
+  }
+  if (event.key === "-") {
+    const btn = document.querySelector("#subtrair");
+    btn.click();
+  }
+  if (event.key === "/") {
+    const btn = document.querySelector("#dividir");
+    btn.click();
+  }
+  if (event.key === "*") {
+    const btn = document.querySelector("#multiplicar");
+    btn.click();
+  }
+  if (event.key === "c") {
+    const btn = document.querySelector("#limpar");
+    btn.click();
+  }
+  if (event.key === "0") {
+    const btn = document.querySelector("#zero");
+    btn.click();
+  }
+  if (event.key === "1") {
+    const btn = document.querySelector("#num1");
+    btn.click();
+  }
+  if (event.key === "2") {
+    const btn = document.querySelector("#num2");
+    btn.click();
+  }
+  if (event.key === "3") {
+    const btn = document.querySelector("#num3");
+    btn.click();
+  }
+  if (event.key === "4") {
+    const btn = document.querySelector("#num4");
+    btn.click();
+  }
+  if (event.key === "5") {
+    const btn = document.querySelector("#num5");
+    btn.click();
+  }
+  if (event.key === "6") {
+    const btn = document.querySelector("#num6");
+    btn.click();
+  }
+  if (event.key === "7") {
+    const btn = document.querySelector("#num7");
+    btn.click();
+  }
+  if (event.key === "8") {
+    const btn = document.querySelector("#num8");
+    btn.click();
+  }
+  if (event.key === "9") {
+    const btn = document.querySelector("#num9");
+    btn.click();
+  }
+  if (event.key === ".") {
+    const btn = document.querySelector("#ponto");
+    btn.click();
+  }
+});
